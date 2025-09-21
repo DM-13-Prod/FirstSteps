1a65242fde27a48db5826f16c4d9326428a903b0
DM-13 <sashasemenenko36@gmail.com>
2025-09-21 15:38:29 +0300

    Fix error

--- First-function.go
+++ First-function.go
@@ -4,7 +4,9 @@ import fmt
 func feature() {
 	fmt.Println("Hello world!")
 	fmt.Println("This is my new function, with my first branch")
+	fmt.Println("This is main branch")
 }
+
 
 
 func merging branches() {
